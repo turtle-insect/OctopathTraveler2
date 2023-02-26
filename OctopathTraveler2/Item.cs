e2b1b070b85001ef1e61f1c896ea707738809128
turtle-insect <turtle.insect@gmail.com>
2023-02-27 06:11:26 +0900

    support Item's param

---
+++ OctopathTraveler2/Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctopathTraveler2
+{
+	internal class Item
+	{
+		private GVAS mGVAS;
+
+		public Item(uint address)
+		{
+			mGVAS = new GVAS();
+
+			for (int i = 0; i < 2; i++)
+			{
+				address = mGVAS.Read(address);
+			}
+		}
+
+		public uint ID
+		{
+			get
+			{
+				UEProperty property = mGVAS.Key("ItemId");
+				return SaveData.Instance().ReadNumber(property.Address, property.Size);
+			}
+		}
+
+		public uint Count
+		{
+			get
+			{
+				UEProperty property = mGVAS.Key("Num");
+				return SaveData.Instance().ReadNumber(property.Address, property.Size);
+			}
+			set
+			{
+				UEProperty property = mGVAS.Key("Num");
+				Util.WriteNumber(property.Address, property.Size, value, 0, 99);
+			}
+		}
+	}
+}
--- OctopathTraveler2/ViewModel.cs
+++ OctopathTraveler2/ViewModel.cs
@@ -10,17 +10,30 @@ namespace OctopathTraveler2
 	internal class ViewModel
 	{
 		public ObservableCollection<Character> Party { get; set; } = new ObservableCollection<Character>();
+		public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
 
 		public ViewModel()
 		{
 			SaveData save = SaveData.Instance();
 			var addresses = save.FindAddress("SavePlayerCharacterData", 0);
-			if (addresses.Count == 0) return;
-			foreach (var address in save.FindAddress("CharacterID", addresses[0]))
+			if (addresses.Count != 0)
 			{
-				var character = new Character(address);
-				//if (character.ID < 0 || character.ID > 8) continue;
-				Party.Add(character);
+				foreach (var address in save.FindAddress("CharacterID", addresses[0]))
+				{
+					var character = new Character(address);
+					//if (character.ID < 0 || character.ID > 8) continue;
+					Party.Add(character);
+				}
+			}
+
+			addresses = save.FindAddress("ItemList", 0);
+			if (addresses.Count != 0)
+			{
+				foreach (var address in save.FindAddress("ItemId", addresses[0]))
+				{
+					var item = new Item(address);
+					Items.Add(item);
+				}
 			}
 		}
 	}
